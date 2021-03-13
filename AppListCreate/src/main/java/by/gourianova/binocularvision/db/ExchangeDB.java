0d4a386bc303e099c259b9380c0bb592c456149d
Tatiana-Gourianova <62667085+Tatiana-Gourianova@users.noreply.github.com>
2021-03-13 16:12:08 +0300

    Add files via upload

--- AppListCreate/src/main/java/by/gourianova/binocularvision/db/ExchangeDB.java
+++ AppListCreate/src/main/java/by/gourianova/binocularvision/db/ExchangeDB.java
@@ -3,12 +3,71 @@ package by.gourianova.binocularvision.db;
 
 //import by.gourianova.binocularvision.repository.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
 //TODO:fix or redone
 
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//JDBC:
+//@Slf4j
 public class ExchangeDB {
+    //TODO: rewrite for site
+/*
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/apptrainer";
+        String username = "root";
+        String password = "778899";
+        try {
+            //     Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            LocalDate date =  LocalDateTime.now().toLocalDate();
+            System.out.println(date+"\n");
+
+           statement.executeUpdate("create table if not exists users(id INT(11) NOT NULL auto_increment," +
+                   "Login varchar(10) not null unique , Password varchar(32) not null,First_Name varchar(15) not null, " +
+                   "Last_Name varchar(15) not null, Balance DECIMAL (6,2), Create_time DATETIME,  primary key (id) );");
+
+           Object param = new java.sql.Timestamp(date.getYear());
+           PreparedStatement  preparedStatement=connection.prepareStatement("insert into users SET Create_time=?");
+            preparedStatement.setObject(1,param);
+            statement.executeUpdate("insert into users (Login,Password,First_Name,Last_Name,Balance) values ('tgour','qwe','Tatiana','Gourianova','3.3')");
+
+            ResultSet resultSet = statement.executeQuery("SELECT *FROM users");
+            while (resultSet.next()) {
+                //log.info()
+                for(int i=1;i<=7;i++)
+                    System.out.print(resultSet.getString(i) + " ");
+                    System.out.println();
+
+            }
+            resultSet.close();
+            preparedStatement.close();
+            statement.close();
+
+            if (connection != null) connection.close();
+
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+
+        }  /*finally {
+            try {
+                if (conection != null)
+                    con.close();
+            } catch (SQLException e) {
+                throw new DAOException(e);
+            }*/
+        }
+  //  }
+
+
+
 
  /*       public static final String DB_URL = "jdbc:h2:E:\\new_doc\\Java\\jwd-task01-template_v2\\db\\exchange";
         public static final String DB_Driver = "org.h2.Driver";
@@ -101,5 +160,5 @@ public class ExchangeDB {
         }
 
     }*/
-    }
+
 
